eb1d6c30279c6f225c1c2b82329e342177bb0713
Ozh <ozh@ozh.org>
2025-09-14 18:09:47 +0200

    Update tests

--- tests/test_email.py
+++ tests/test_email.py
@@ -4,32 +4,17 @@ import imaplib
 
 from utils.constants import SMTP_SERVER, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, IMAP_SERVER, OWNER_EMAIL
 from utils.email import tell_owner, tell_sender
+from utils.logs import connection_test_imap, connection_test_smtp
 
 print("Test IMAP connection...")
-try:
-    mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-    mail.login(SMTP_USER, SMTP_PASSWORD)
-    mail.select("INBOX")
-    mail.close()
-    mail.logout()
-    print("IMAP connection successful.")
-except Exception as e:
-    print(f"IMAP connection failed: {e}")
-
-time.sleep(1)
+print(connection_test_imap())
+time.sleep(0.5)
 
 print("Test SMTP connection...")
-try:
-    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp:
-        smtp.login(SMTP_USER, SMTP_PASSWORD)
-        smtp.quit()
-    print("SMTP connection successful.")
-except Exception as e:
-    print(f"SMTP connection failed: {e}")
+print(connection_test_smtp())
+time.sleep(0.5)
 
 print("Sending test email to owner...")
 tell_owner('noreply@test-email.null')
-print("Sending test email to sender...")
-tell_sender(OWNER_EMAIL, 'assets/samples/sample_dithered_image.jpg')
 
 print("End")
